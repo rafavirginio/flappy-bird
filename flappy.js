672c457d0d5ed9275e2cdb1422dc3d066d5954ee
Rafa Virginio <rafael5845@gmail.com>
2021-01-15 13:09:57 -0300

    pushes

---
+++ flappy.js
@@ -0,0 +1,33 @@
+function novoElemento(tagName, className) {
+    const elem = document.createElement(tagName)
+    elem.className = className
+    return elem
+}
+
+function Barreira(reversa = false) {
+    this.elemento= novoElemento ('div','barreira')
+
+
+    const borda = novoElemento('div', 'borda')
+    const corpo = novoElemento('div', 'corpo')
+    this.elemento.appendChild(reversa ? corpo : borda)
+    this.elemento.appendChild(reversa ? borda : corpo)
+
+
+    this.setAltura = altura => corpo.style.height = `${altura}px`
+}
+
+// const b = new Barreira(true)
+// b.setAltura(300)
+// document.querySelector('[wm-flappy]').appendChild(b.elemento)
+
+
+function ParDeBarreiras(altura, abertura, x) {
+    this.elemento = novoElemento ('div', 'par-de-barreiras')
+
+    this.superior = new Barreira(true)
+    this.inferior = new Barreira(false)
+
+
+    this.elemento.appendChild (this.superior.elemento)
+}
