aa1b7e9396fc30bb8760668d42fcd0f715965904
Rafa Virginio <rafael5845@gmail.com>
2021-01-16 11:44:17 -0300

    Update flappy.js

--- flappy.js
+++ flappy.js
@@ -35,7 +35,7 @@ function ParDeBarreiras(altura, abertura, x) {
     this.inferior.setAltura(alturaInferior)
   }
 
-  this.getX = () => parseInt(this.elemento.style.left.split("px")[0]);
+  this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
   this.setX = (x) => (this.elemento.style.left = `${x}px`)
   this.getLargura = () => this.elemento.clientWidth
 
